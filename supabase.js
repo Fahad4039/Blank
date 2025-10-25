0a22976a7576fed0f4198d186190c3910586df67
Fahad4039 <135491469+Fahad4039@users.noreply.github.com>
2025-10-25 09:47:01 +0500

    Update Supabase client URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://YOUR-PROJECT-URL.supabase.co",
-  "YOUR-ANON-KEY"
+  "https://zahdxkksazgvqaskpcla.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphaGR4a2tzYXpndnFhc2twY2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMTY3NzgsImV4cCI6MjA3Njg5Mjc3OH0.yNNINmbhLbMOEOQmH3Dpo7E3xwPACK-ji0BQFPYZ8xk"
 );
